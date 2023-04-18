540da1a6dcf8eb6dd7af371ef6c140622847a10d
kispruki <kispruki@gmail.com>
2023-04-18 11:36:05 +0200

    up ot date

--- Program.cs
+++ Program.cs
@@ -28,3 +28,18 @@
         Console.WriteLine(avg / this.score.Count);
     }
 }
+
+
+void bubble_sort(ref List<int> arr){
+    int n = arr.Count;
+    
+    for (int i = 0; i<n-1; i++){
+        for (int j = 0; j<n-i-1; j++){
+            if (arr[j] < arr[j+1]){
+                int c = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = c;
+            }
+        }
+    }
+}
